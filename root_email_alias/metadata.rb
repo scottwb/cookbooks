485318c30b49030376eb1dc306f9193942791121
Scott W. Bradley <scottwb@scottwb.net>
2011-04-27 13:28:23 -0700

    Bumped root_email_alias cookbook from 0.0.1 to 0.0.2

--- root_email_alias/metadata.rb
+++ root_email_alias/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "scottwb@gmail.com"
 license          "Apache 2.0"
 description      "Sets a root email alias in /etc/aliases"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 
 recipe "root_email_alias", "Sets a root email alias in /etc/aliases"
 
