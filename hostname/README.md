8d34d3df58da52d55d92a9f3849ad35a79aa72dd
Scott W. Bradley <scottwb@scottwb.net>
2014-01-20 12:55:11 -0800

    Finally cleanup and commit a simple `hostname` cookbook I've been carting aroudn for a few years that just sets the node's hostname to it's chef node name.

---
+++ hostname/README.md
@@ -0,0 +1,13 @@
+Description
+===========
+Sets the node's hostname to the Chef node it was created with.
+
+Requirements
+============
+
+Attributes
+==========
+
+Usage
+=====
+Include `recipe[hostname]` in your runlist.
---
+++ hostname/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Scott W. Bradley"
+maintainer_email "scottwb@gmail.com"
+license          "Apache 2.0"
+description      "Set's node's hostname to the Chef node name it was created with."
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1"
